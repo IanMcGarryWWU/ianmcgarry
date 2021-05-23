901e5a1f3a45e5c84b37b57480664d74160e4faa
Ian McGarry <36414826+IanMcGarryWWU@users.noreply.github.com>
2021-05-23 11:16:09 -0400

    Links not working
    Removed Writing page added links but can't click on them

--- src/App.js
+++ src/App.js
@@ -85,7 +85,7 @@ const App = () => {
             setArrowY(arrowY - (((arrowY - scrollY) * arrowspeed) / 50))
         } else if (arrowY < scrollY) {
             let moveamount = (((scrollY - arrowY) * arrowspeed) / 50)
-            if (scrollY > (3.5 * height)) {
+            if (scrollY > (2.5 * height)) {
                 moveamount = 0
             }
             setArrowY( arrowY + moveamount )
@@ -195,11 +195,9 @@ const App = () => {
                       <div className="HeaderItem">
                           <HeightVary className="GlassCard" text="Skills" scrollY={scrollY} height={height} section={3}/>
                       </div>
+
                       <div className="HeaderItem">
-                          <HeightVary className="GlassCard" text="Writing" scrollY={scrollY} height={height} section={4}/>
-                      </div>
-                      <div className="HeaderItem">
-                          <HeightVary className="GlassCard" text="Contact Me" scrollY={scrollY} height={height} section={5}/>
+                          <HeightVary className="GlassCard" text="Contact Me" scrollY={scrollY} height={height} section={4}/>
                       </div>
               </div>
           </div>
@@ -267,19 +265,25 @@ const App = () => {
           <div style={{ position: 'absolute', height:"100%",  width:"100%"}} >
               <MyParticles height={"100%"}/>
           </div>
-          <div className={"Writing"}>
-              Writing
-          </div>
+
           <div className={"Contact"}>
               <div>
               <div>
-              <svg xmlns="http://www.w3.org/2000/svg" width="240" height="240" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11
+              <svg  xmlns="http://www.w3.org/2000/svg" width="240" height="240" viewBox="0 0 24 24">
+                  <a className={'SvgAnchor'} href="https://www.linkedin.com/in/ian-mcgarry-tech/">
+                  <path className={"ContactSVG"} d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11
               19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75
-              1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg>
+              1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
+                  </a>
+                  </svg>
                   </div>
               <div>
-              <svg xmlns="http://www.w3.org/2000/svg" width="240" height="240" viewBox="0 0 24 24"><path d="M0 3v18h24v-18h-24zm6.623 7.929l-4.623 5.712v-9.458l4.623 3.746zm-4.141-5.929h19.035l-9.517
-              7.713-9.518-7.713zm5.694 7.188l3.824 3.099 3.83-3.104 5.612 6.817h-18.779l5.513-6.812zm9.208-1.264l4.616-3.741v9.348l-4.616-5.607z"/></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="240" height="240" viewBox="0 0 24 24">
+                  <a className={'SvgAnchor'}  href = "mailto: contact@ianmcgarry.tech">
+                  <path className={"ContactSVG"} d="M0 3v18h24v-18h-24zm6.623 7.929l-4.623 5.712v-9.458l4.623 3.746zm-4.141-5.929h19.035l-9.517
+              7.713-9.518-7.713zm5.694 7.188l3.824 3.099 3.83-3.104 5.612 6.817h-18.779l5.513-6.812zm9.208-1.264l4.616-3.741v9.348l-4.616-5.607z"/>
+                  </a>
+                  </svg>
                   </div>
               </div>
           </div>
